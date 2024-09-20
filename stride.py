27f82d560015d1f23c22a2616393240452ea373d
tkreusel <tobias.kreusel@t-online.de>
2024-09-21 00:09:48 +0200

    fixed stride function

--- stride.py
+++ stride.py
@@ -1,14 +1,14 @@
-def stride(pdb_file, stride_dir = './stride/stride', output_dir = './data/strideout', save = False, modify_pdb = False, os = 'windows'):
+def stride(pdb_file, stride_dir = './stride/stride', output_dir = './data/strideout', save = False, modify_pdb = False, system = 'windows'):
     """
     This function runs the Stride program on a PDB file and returns the secondary structure information and solvent accessable area of each residue. The output is a pandas dataframe. If save = True, the output is saved as a csv file in the specified output directory.
     """
     import subprocess
     import pandas as pd
     import os
-    if os == 'windows':
-        result = subprocess.run(['wsl', './stride/stride', pdb_file], stdout=subprocess.PIPE, text=True)
-    elif os == 'mac':
-        result = subprocess.run(['./stride/stride', pdb_file], stdout=subprocess.PIPE, text=True)
+    if system == 'windows':
+        result = subprocess.run(['wsl', stride_dir, pdb_file], stdout=subprocess.PIPE, text=True)
+    elif system == 'mac':
+        result = subprocess.run([stride_dir, pdb_file], stdout=subprocess.PIPE, text=True)
     secondary = [line for line in result.stdout.splitlines() if line.startswith('ASG')]
     data_str = "n".join(secondary)
     if len(secondary[0].split()) == 10:
@@ -34,12 +34,12 @@ def stride(pdb_file, stride_dir = './stride/stride', output_dir = './data/stride
             f.truncate()
     return secondary_df
 
-def no_secondary(pdb_file, stride_dir = './stride/stride', output_dir = './data/strideout', save = False, modify_pdb = False, os = 'windows'):
+def no_secondary(pdb_file, stride_dir = './stride/stride', output_dir = './data/strideout', save = False, modify_pdb = False, system = 'windows'):
     """
     This function returns the residues that are not part of an alpha helix or beta sheet. The output is a pandas dataframe of only those amino acids.
     """
     from stride import stride
-    secondary_df = stride(pdb_file, stride_dir=stride_dir, output_dir=output_dir, save=save, modify_pdb=modify_pdb, os=os)
+    secondary_df = stride(pdb_file, stride_dir=stride_dir, output_dir=output_dir, save=save, modify_pdb=modify_pdb, system = system)
     no_sec = secondary_df[secondary_df['code'].isin(['C', 'T'])]
     return no_sec
 
